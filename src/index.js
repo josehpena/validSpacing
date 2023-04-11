a4eff371ab68d268cd17638a37ecfcc110c530d5
Jose Pena <>
2023-04-10 21:54:15 -0300

    Returning false if has space at end or start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const validSpacing = (str) => {
     if(!str.includes(' '))
         return true;
     else if(str.length == 1)
-        return false
+        return false;
+    else if(str[0].includes(' ') || str[str.length - 1].includes(' '))
+        return false;
 }
 
 module.exports = {
