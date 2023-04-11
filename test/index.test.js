10db7f3ea7b13bfbe0d3833ddef612c598c20096
Jose Pena <>
2023-04-10 21:38:58 -0300

    Second test created

--- test/index.test.js
+++ test/index.test.js
@@ -6,4 +6,10 @@ describe('validSpacing', () =>{
 
         expect(result).toBe(true);
     });
+
+    it('Should return false if has only a space without any word', () =>{
+        let result = validSpacing(' ');
+
+        expect(result).toBe(false);
+    })
 })
